c1b2ae0996f7c54f65d9b85d632eedbe671a504e
Grace Noah <gracenoahgh@gmail.com>
2020-01-06 15:36:53 -0500

    use a pure go blake2b library

--- hash.go
+++ hash.go
@@ -2,15 +2,15 @@ package zecutil
 
 import (
 	"github.com/btcsuite/btcd/chaincfg/chainhash"
-	"github.com/codahale/blake2"
+	"github.com/dchest/blake2b"
 )
 
 // blake2bHash zcash hash func
 func blake2bHash(data, key []byte) (h chainhash.Hash, err error) {
-	bHash := blake2.New(&blake2.Config{
-		Size:     32,
-		Personal: key,
-	})
+	bHash, err := blake2b.New(&blake2b.Config{Size: 32, Person: key})
+	if err != nil {
+		return h, err
+	}
 
 	if _, err = bHash.Write(data); err != nil {
 		return h, err
